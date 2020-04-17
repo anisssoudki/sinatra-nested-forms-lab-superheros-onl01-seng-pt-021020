090e2cb3fd0b87f022bbb7ecbcd8d9194ddb77b2
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-17 19:19:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
 
-    get '/' do 
+ get '/' do
       erb :super_hero
     end
-    
-     post '/teams' do
+
+
+    post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
 
@@ -20,4 +21,4 @@ class App < Sinatra::Base
 
       erb :team
     end
-  end	
+end	end
